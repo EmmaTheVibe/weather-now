18214b8f4b56b0056fe05277c4f0ebef160a33e1
EmmaTheVibe <peteremmanuel316@gmail.com>
2025-10-24 22:34:33 +0100

    fix

--- app/_components/Weather/Weather.jsx
+++ app/_components/Weather/Weather.jsx
@@ -1,9 +1,14 @@
+import { Suspense } from "react";
 import WeatherClient from "./WeatherClient";
 import styles from "./Weather.module.css";
+import JumpingDots from "../JumpingDots/JumpingDots";
+
 export default function Weather() {
   return (
     <section className={styles.weather}>
-      <WeatherClient />
+      <Suspense fallback={<JumpingDots />}>
+        <WeatherClient />
+      </Suspense>
     </section>
   );
 }
