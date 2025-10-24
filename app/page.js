17b92e2fe97f5c171cb355438d3e04b378bfc6e4
EmmaTheVibe <peteremmanuel316@gmail.com>
2025-10-24 22:41:57 +0100

    fix

--- app/page.js
+++ app/page.js
@@ -1,11 +1,17 @@
+import { Suspense } from "react";
 import NavBar from "./_components/NavBar/NavBar";
 import Weather from "./_components/Weather/Weather";
+import JumpingDots from "./_components/JumpingDots/JumpingDots";
 
 export default function page() {
   return (
     <>
-      <NavBar />
-      <Weather />
+      <Suspense fallback={<JumpingDots />}>
+        <NavBar />
+      </Suspense>
+      <Suspense fallback={<JumpingDots />}>
+        <Weather />
+      </Suspense>
     </>
   );
 }
